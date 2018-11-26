81688ad234819f3e46abcc6edbf1640d20da5ae9
dcs0spb <s.p.bradley@durham.ac.uk>
2018-11-26 14:54:30 +0000

    properties and draw function associated with object

--- p5_glass.js
+++ p5_glass.js
@@ -3,11 +3,15 @@ let glass = {
     centre_x : 150,
     centre_y : 250};
 
+glass.draw = function(){
+    ellipse(this.centre_x, this.centre_y, this.width, this.width);
+    line(this.centre_x, this.centre_y + this.width/2, this.centre_x, this.centre_y + 2* this.width);
+}
+
 function setup(){
     createCanvas(500, 500);
 }
 
 function draw(){
-    ellipse(glass.centre_x, glass.centre_y, glass.width, glass.width);
-    line(glass.centre_x, glass.centre_y + glass.width/2, glass.centre_x, glass.centre_y + 2* glass.width);
+    glass.draw();
 }
